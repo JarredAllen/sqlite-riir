8513c7367a68ef664f93dad122da92bb83e5ad21
Jarred Allen <jarredallen73@gmail.com>
2024-09-04 13:21:28 -0700

    Update table iterator to step through internal pages

--- src/table_iter.rs
+++ src/table_iter.rs
@@ -43,7 +43,34 @@ impl<'a> Iterator for TableIter<'a> {
             .read_page(top_frame.page_num)
             .expect("Error reading pages");
         match page.parse() {
-            ParsedPage::BTreeTableInternal(_) => todo!("Walk the b-tree"),
+            ParsedPage::BTreeTableInternal(internal) => {
+                // If the top page is an internal node, we set the top of the stack to the next
+                // page to look in, and then recurse.
+                if let Some(cell) = internal.cells().nth(top_frame.idx_in_page) {
+                    top_frame.idx_in_page = top_frame.idx_in_page.saturating_add(1);
+                    self.stack.push(StackFrame {
+                        page_num: cell.left_child_page as usize,
+                        idx_in_page: 0,
+                    });
+                } else if top_frame.idx_in_page == internal.num_cells() {
+                    top_frame.idx_in_page = top_frame.idx_in_page.saturating_add(1);
+                    // Minor optimization: this is the last child, so we can replace the top stack
+                    // frame with its child instead of pushing it on top.
+                    *top_frame = StackFrame {
+                        page_num: internal.rightmost_child_idx() as usize,
+                        idx_in_page: 0,
+                    };
+                } else {
+                    // We're past the end of this page, so we remove the top frame and recurse into
+                    // the parent (should never be hit because of above optimization).
+                    self.stack.pop();
+                }
+                // We have a different stack frame on top (pointing to a different page), so we can
+                // recurse. This should get tail-call optimization, but even if it doesn't, we only
+                // have a pointer on the stack at this point, so it shouldn't explode the stack too
+                // much.
+                self.next()
+            }
             ParsedPage::BTreeTableLeaf(leaf) => {
                 let Some(cell) = leaf.cells().nth(top_frame.idx_in_page) else {
                     self.stack.pop();
@@ -95,6 +122,20 @@ mod tests {
                 .expect("Failed to make iterator")
                 .count(),
             0,
+        );
+    }
+
+    #[test]
+    fn test_two_layers_tree() {
+        let mut db = Database::new(
+            File::open("./test-data/many-tables.sqlite").expect("Failed to open database file"),
+        )
+        .expect("Failed to parse database file as database");
+        assert_eq!(
+            TableIter::new(&mut db, "sqlite_schema")
+                .expect("Failed to make iterator")
+                .count(),
+            1024,
         );
     }
 }
